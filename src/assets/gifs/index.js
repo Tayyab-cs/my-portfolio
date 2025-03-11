bb65ebaaae15530ebe9be3d8f3c214594ecbb37d
Tayyab-cs <muhammadtayyab.cs@gmail.com>
2025-03-12 00:32:21 +0500

    first commit

---
+++ src/assets/gifs/index.js
@@ -0,0 +1,31 @@
+import htmlGif from './html-5.gif';
+import cssGif from './CSS.gif';
+import chakraGif from './ChakraUI.gif';
+import expressGif from './ExpressJS.gif';
+import gitGif from './Git.gif';
+import gitHubGif from './GitHub.gif';
+import jsGif from './JS.gif';
+import mongoGif from './MongoDB.gif';
+import mySqlGif from './MySQL.gif';
+import nodeGif from './NodeJS.gif';
+import postmanGif from './Postman.gif';
+import reactGif from './ReactJS.gif';
+import tailwindGif from './TailwindCSS.gif';
+import vscodeGif from './VSCode.gif';
+
+export {
+  htmlGif,
+  cssGif,
+  chakraGif,
+  expressGif,
+  gitGif,
+  gitHubGif,
+  jsGif,
+  mongoGif,
+  mySqlGif,
+  nodeGif,
+  postmanGif,
+  reactGif,
+  tailwindGif,
+  vscodeGif,
+};
